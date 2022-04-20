2c0b8d41f22e05dece2e67c29ecf8dbd241fadf5
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-04-20 17:25:36 +0500

    finished two exercises

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Problem 210
+
+/*
+let n = 155;
+let a = 2;
+
+console.log(convertIt(n, a));
+
+function convertIt(n, a){
+  if(a == 2){
+    return n.toString((2));
+  } else if(a == 8){
+    return n.toString((8));
+  } else if(a == 16){
+    return n.toString((16));
+  }
+}
+*/
+
+// Problem #211
+
+/*
+let row = "Py🐍on";
+
+console.log(spreadIt(...row));
+
+function spreadIt(...spreadIt){
+  return spreadIt;
+}
+*/
+
+// Problem 212
