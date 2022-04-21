8b1e7f0a2602efaea527fbd91396d6367a76df86
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-04-21 08:00:17 +0500

    solved the last three problems

--- js/main.js
+++ js/main.js
@@ -30,3 +30,77 @@ function spreadIt(...spreadIt){
 */
 
 // Problem 212
+
+/*
+let n = "314";
+let a = 8;
+
+console.log(convertIt(n, a));
+
+function convertIt(n, a){
+  if(a == 16){
+    return n = parseInt(n, 16);
+  } else if(a == 2){
+    return n = parseInt(n, 2);
+  } else if (a == 8){
+    return n = parseInt(n, 8);
+  }
+}
+*/
+
+// Problem #213
+
+/*
+let arr = [7, 11, 17, 23, 31, 37];
+let newArr = [];
+
+console.log(getPalindromeNumbersAsArray(arr));
+
+function getPalindromeNumbersAsArray(arr){
+  for(let i = 0; i < arr.length; i++){
+    let check = 0;
+    for(let j = 2; j < arr[i]; j++){
+      if(arr[i] % j == 0){
+        check++;
+      }
+    }
+    if(check == 0){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+*/
+
+// Problem #214
+
+/*
+let arr = [7, 11, 7, 23, 31, 37];
+let newArr = [];
+
+console.log(getPalindromeNumbersAsArray(arr));
+
+function getPalindromeNumbersAsArray(arr){
+  for(let i = 0; i < arr.length; i++){
+    let v = isPalindromeNumber(String(arr[i]));
+    if(v !== undefined){
+      newArr.push(v);
+    }
+  }
+  return newArr;
+}
+
+
+function isPalindromeNumber(numString){
+  let c = numString.length - 1;
+  let check = true;
+  for(let i = 0; i < numString.length / 2; i++, c--){
+    if(numString[i] != numString[c]){
+      check = false;
+    }
+  }
+  if(check == true){
+    return numString;
+  } 
+}
+*/
